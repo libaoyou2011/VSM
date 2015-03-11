fb495ef42ab8d345dfd834265e2823cf42d0806a
EdwardDing <nsobject.ding.jiarui@gmail.com>
2015-03-11 13:06:47 +0800

    处理文本库
    从比较两个文本改为将Query与文本库中所有文本比较

--- src/main/scala/VSM.scala
+++ src/main/scala/VSM.scala
@@ -1,6 +1,5 @@
 // 丁嘉瑞
-// version：1.0
-// 功能基本完成，没有进行流水并行设计
+// version：1.1
 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
@@ -14,35 +13,28 @@ def main(args: Array[String]): Unit = {
 
 		// 预处理doc，获取doc向量的距离
 		// 这一部分应当另写程序处理，并将doc库的距离统一放到一个文件中
-		val doc = sc.textFile("/Users/EdwardDing/Documents/SCALA/VSM/doc_single").flatMap(line => line.split(" "))
-		val sumD = doc.map(number => number.toDouble * number.toDouble).reduce(_ + _)
-		val distD = Math.sqrt(sumD)
+		val doc = sc.textFile("/Users/EdwardDing/VSM/doc").map(line => line.split(" ")).collect
+		val sumD = 
+		for (line <- doc)
+			yield line.map(s => s.toDouble * s.toDouble).reduce(_ + _)
+		val distD = sumD.map(x => Math.sqrt(x))
 
 		// 处理query的距离
 		// 仅需处理一次
-		val query = sc.textFile("/Users/EdwardDing/Documents/SCALA/VSM/query").flatMap(line => line.split(" "))
-		val sumQ = query.map(number => number.toDouble * number.toDouble).reduce(_ + _)
+		val query = sc.textFile("/Users/EdwardDing/VSM/query").flatMap(line => line.split(" ")).collect
+		val sumQ = query.map(s => s.toDouble * s.toDouble).reduce(_ + _)
 		val distQ = Math.sqrt(sumQ)
 
 		// 计算分母
-		val denom = distQ * distD
+		val denom = distD.map(x => x * distQ)
 
-		if (doc.count != query.count) {
-			println("### ERROR: Part of data is missing.")
-		}
-		else 
-		{
-			// 计算VSM算法中的分子部分
-			val tempZip = query.zip(doc)
-			val numer = tempZip.map(pair => pair._1.toDouble * pair._2.toDouble).reduce(_ + _)
+		// 计算分子
+		val numer = 
+		for (line <- doc)
+			yield line.zip(query).map(pair => pair._1.toDouble * pair._2.toDouble).reduce(_ + _)
 
-			// 得到相似度结果
-			val sim = numer / denom
-			println("### OUTPUT: The similarity is: " + sim)
-		}
-	}
-
-	def mapFun(line: String) = {
-		
+		// 计算相似度
+		val sim = numer.zip(denom).map(pair => pair._1 / pair._2)
+		sim.foreach(x => println(f"### Output: Similarity = $x%.3f"))
 	}
 }
